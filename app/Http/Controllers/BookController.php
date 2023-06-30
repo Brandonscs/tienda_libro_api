cf41228fd393ecee77f97a65b9e984684bda923d
Brandonscs <135081506+Brandonscs@users.noreply.github.com>
2023-06-29 22:43:57 -0500

    se creo la primera captura de errores

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,15 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use App\Models\Book;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
 
 class BookController extends Controller
 {
     public function index()
     {
-        $books = Book::all();
+        try {
+            $books = Book::all();
 
-        return $books;
+            return response()->json(['success' => true, 'data' => $books]);
+        } catch (Exception $e) {
+            Log::error($e->getMessage() . ' line: ' . $e->getLine() . ' file: ' . $e->getFile());
+
+            return response()->json([
+                'success' => false,
+                'message' => 'Error de servidor',
+                'info' => $e->getMessage()
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 }
