c3b09115c8d7fdde92bce0ac96a5f25e24bcf3a9
Brandonscs <135081506+Brandonscs@users.noreply.github.com>
2023-07-01 00:02:02 -0500

    se creo el CRUD para books

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -19,11 +19,75 @@ class BookController extends Controller
         } catch (Exception $e) {
             Log::error($e->getMessage() . ' line: ' . $e->getLine() . ' file: ' . $e->getFile());
 
-            return response()->json([
-                'success' => false,
-                'message' => 'Error de servidor',
-                'info' => $e->getMessage()
-            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+            return response()->json(['success' => false, 'message' => 'Error de servidor', 'info' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function show($id)
+    {
+        try {
+            $book = Book::find($id);
+            if (empty($book)) {
+                return response()->json(['success' => false, 'message' => 'No existe este libro'], Response::HTTP_NOT_FOUND);
+            } else {
+                return response()->json(['success' => true, 'data' => $book]);
+            }
+
+        } catch (Exception $e) {
+            Log::error($e->getMessage() . ' line: ' . $e->getLine() . ' file: ' . $e->getFile());
+
+            return response()->json(['success' => false, 'message' => 'Error de servidor', 'info' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            $book = Book::create($request->all());
+
+            return response()->json(['success' => true, 'message' => 'Libro creado.', 'data' => $book], Response::HTTP_CREATED);
+        } catch (Exception $e) {
+            Log::error($e->getMessage() . ' line: ' . $e->getLine() . ' file: ' . $e->getFile());
+
+            return response()->json(['success' => false, 'message' => 'Error de servidor', 'info' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        try {
+            $book = Book::find($id);
+
+            if (empty($book)) {
+                return response()->json(['success' => false, 'message' => 'No existe este libro'], Response::HTTP_NOT_FOUND);
+            }
+
+            $book->update($request->all());
+
+            return response()->json(['success' => true, 'message' => 'Libro actualizado.', 'data' => $book], Response::HTTP_CREATED);
+        } catch (Exception $e) {
+            Log::error($e->getMessage() . ' line: ' . $e->getLine() . ' file: ' . $e->getFile());
+
+            return response()->json(['success' => false, 'message' => 'Error de servidor', 'info' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function destroy($id)
+    {
+        try {
+            $book = Book::find($id);
+
+            if (empty($book)) {
+                return response()->json(['success' => false, 'message' => 'No existe este libro'], Response::HTTP_NOT_FOUND);
+            } 
+
+            $book->delete();
+
+            return response()->json(null, Response::HTTP_NO_CONTENT);
+        } catch (Exception $e) {
+            Log::error($e->getMessage() . ' line: ' . $e->getLine() . ' file: ' . $e->getFile());
+
+            return response()->json(['success' => false, 'message' => 'Error de servidor', 'info' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -27,5 +27,5 @@ Route::prefix('v1')->group(function() {
         ]);
     });
 
-    Route::get('books', [BookController::class, 'index']);
+    Route::apiResource('books', BookController::class);
 });
